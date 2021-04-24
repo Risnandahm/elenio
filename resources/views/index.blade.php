40a3304b98889cecfb3f4e723f25fee7b2a2dd7b
risnandahm <ndhajh10@gmail.com>
2021-04-24 22:16:17 +0700

    make it better

---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layout/main')
+
+@section('title', 'MIN NANDA KECE')
+
+@section('container')
+
+	<div class="container">
+		<div class="row">
+			<div class="col-10">
+				        <div class="spinner-border text-primary" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-secondary" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-success" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-danger" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-warning" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-info" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-dark" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+				<div class="card">
+  <div class="card-header"> CLASS 6A
+  	방탄소년단; RR: Bangtan Sonyeondan; Min yoongi !!✧
+  </div>
+	<h1 class="mt-2">
+    18090092 | Risnanda Hasna Mufida</h1>
+<img src="{{asset('yoongi.gif') }}" height="300px" width="350px;"/>
+  <div class="card-body">
+    <h5 class="card-title">gummy smile :D</h5>
+		</div>
+	</div>
+	 </div>
+</div>
+@endsection
